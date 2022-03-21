25bb5e405a6dfb17946d2810af41f4e76f8ec31e
AliceDarkmoon55 <44077088+AliceDarkmoon55@users.noreply.github.com>
2022-03-21 15:09:31 +0700

    Finally logger

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import Exceptions.CantCreateCommand;
+import Exceptions.CantOpenConfigFile;
+import Exceptions.InvalidNumberOfArguments;
+import StackCalculator.StackCalculator;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+
+public class Main {
+    static private final Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) throws IOException, InvalidNumberOfArguments, CantOpenConfigFile, CantCreateCommand {
+        String path = null;
+        if (args.length > 1){
+            throw new InvalidNumberOfArguments("More than 1 element",  1, args.length);
+        }
+        else if (args.length == 1){
+            path = args[0];
+        }
+        final String loggerConfigFileName = "/config/logger.properties";
+        try{
+            LogManager.getLogManager().readConfiguration(Main.class.getResourceAsStream(loggerConfigFileName));
+        }
+        catch (IOException ex){
+            logger.log(Level.SEVERE, "Exception: ", ex);
+            throw new CantOpenConfigFile("Cant open logger config file", loggerConfigFileName);
+        }
+        StackCalculator calc = new StackCalculator(path);
+        calc.makeCount();
+    }
+}
